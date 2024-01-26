9dc19bcddd8568deb68b866c9611c74d3d49152a
Joris Bailly <joris.bailly.2001@gmail.com>
2024-01-26 14:13:55 +0100

    Test function should return 1 with negative grades

--- calculAverageGrades.test.js
+++ calculAverageGrades.test.js
@@ -1,6 +1,12 @@
 const calculAverageGrades =  require('./calculAverageGrades');
-test('Calcul average of grades with 4 and 5 should give 4.5', () => {
+test('Calculate average of grades with 4 and 5 should give 4.5', () => {
     const grades = [4,5];
     const average = calculAverageGrades(grades);
     expect(average).toBe(4.5);
+});
+
+test('Calculates average  with negative grades should return 1', () => {
+    const grades = [-10, -5, -1 , -2];
+    const average = calculAverageGrades(grades);
+    expect(average).toBe(1);
 });
