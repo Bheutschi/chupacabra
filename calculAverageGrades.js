5f66ef0ef36a10de802d50dff41a7be0f70c23b5
Joris Bailly <joris.bailly.2001@gmail.com>
2024-01-26 14:19:22 +0100

    Test function should return 1 with negative grades now working
    Add logic into the function to transform all negative grade equal to 1 and add it to sum. Because there is not negative grade given at school an we can have less than 1

--- calculAverageGrades.js
+++ calculAverageGrades.js
@@ -2,7 +2,15 @@ function calculAverageGrades(grades) {
     if (grades.length === 0) {
         return 0;
     }
-    const sum = grades.reduce((acc, note) => acc + note, 0);
+    let sum = 0
+    for(let i = 0; i < grades.length; i++) {
+        const grade = grades[i];
+        if (grade < 0) {
+            sum += 1;
+        }else{
+            sum += grade;
+        }
+    }
     return sum / grades.length;
 }
 module.exports = calculAverageGrades;
