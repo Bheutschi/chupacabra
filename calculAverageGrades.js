a5dea34560d007519e430cefc6b4b8284c544961
Joris Bailly <joris.bailly.2001@gmail.com>
2024-01-28 10:59:27 +0100

    Test When a grade is more than 6 should return the error answer  adding code logic
    Add in the function a verification if a grade is more than 6 it stop the code to return an error

--- calculAverageGrades.js
+++ calculAverageGrades.js
@@ -1,9 +1,22 @@
+// calculMoyenne.js
 function calculAverageGrades(grades) {
     if (grades.length === 0) {
         return 0;
     }
+
+    for (let i = 0; i < grades.length; i++) {
+        const grade = grades[i];
+
+        if (grade > 6) {
+            console.warn("You shouldn't put a grade more than 6");
+            return "You shouldn't put a grade more than 6";
+        }
+    }
+
     const adjustedGrades = grades.map(grade => (grade < 0 ? 1 : grade));
+
     const sum = adjustedGrades.reduce((acc, note) => acc + note, 0);
     return sum / grades.length;
 }
+
 module.exports = calculAverageGrades;
--- calculAverageGrades.test.js
+++ calculAverageGrades.test.js
@@ -12,7 +12,7 @@ test('Calculates average  with negative grades should return 1', () => {
 });
 
 test("When a grade is more than 6 should return the error answer", () => {
-    const grades = [7];
+    const grades = [2,3,4,7];
     const average = calculAverageGrades(grades);
     expect(average).toMatch("You shouldn't put a grade more than 6");
 });
